435bdb9ca5deaea246dfd58365d69bc4165412d8
adlerSousa <adler.amorim@itixti.com>
2025-11-17 16:51:09 -0300

    Compartilhamento de dados entre componentes pais e filhos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FirstComponent } from './components/first-component/first-component';  
+import { FirstComponent } from './components/first-component/first-component';
+import { ParentData } from "./components/parent-data/parent-data";  
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FirstComponent],
+  imports: [RouterOutlet, FirstComponent, ParentData],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  userName = 'Joaquim';
+
+  userData={
+    email :'joaqui@gmail.com',
+    role : 'Admin'
+  };
   protected readonly title = signal('curso-angular');
 }

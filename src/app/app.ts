502eb7b31f48568be717af77d413fe4a06a40537
adlerSousa <adler.amorim@itixti.com>
2025-11-18 08:37:34 -0300

    Aula - Renderização condicional

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponent } from './components/first-component/first-component';
 import { ParentData } from "./components/parent-data/parent-data";
-import { Directives } from "./components/directives/directives";  
+import { Directives } from "./components/directives/directives";
+import { IfRender } from "./components/if-render/if-render";  
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FirstComponent, ParentData, Directives],
+  imports: [RouterOutlet, FirstComponent, ParentData, Directives, IfRender],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/if-render/if-render.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+@Component({
+  selector: 'app-if-render',
+  imports: [NgIf],
+  templateUrl: './if-render.html',
+  styleUrl: './if-render.css',
+})
+export class IfRender {
+
+  canShow : boolean = true;
+  name: string = 'Joao';
+
+}
